7caa31afa7eee1d8a261219d2319b5d67eb3f4ad
dingmichael9 <ding.michael9@gmail.com>
2021-03-21 22:38:11 -0400

    Add validation for positive amounts

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
     belongs_to :sender, class_name: "User"
     belongs_to :receiver, class_name: "User"
     
-    validates :amount, numericality: true
+    validates :amount, numericality: {greater_than: 0}
     validates :sender_id, presence: true
     validates :receiver_id, presence: true
     validates :approved, inclusion: {in: [ true, false ]}
